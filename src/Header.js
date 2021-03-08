2c6399435ccfc2d171e819e75586076ba7b20eb2
Melissa Locquiao <ml13sj@brocku.ca>
2021-03-08 15:26:06 -0500

    testing image import

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import './styles/Header.css';
-import logo from './assets/logo.png';
+import logo from './logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
